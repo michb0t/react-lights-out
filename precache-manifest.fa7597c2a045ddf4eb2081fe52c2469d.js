fc9f2d559daccc1b3d3584dd1cd0644d0f0c4ed0
michb0t <t.pham29@live.com.au>
2022-04-25 15:43:39 +1000

    Updates

---
+++ precache-manifest.fa7597c2a045ddf4eb2081fe52c2469d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a2c3ce4a2e88423ccfd",
+    "url": "/react-lights-out/static/css/main.123e5b87.chunk.css"
+  },
+  {
+    "revision": "5a2c3ce4a2e88423ccfd",
+    "url": "/react-lights-out/static/js/main.5a2c3ce4.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/react-lights-out/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "bc442d166c0a4c89638d2b751b7d34d5",
+    "url": "/react-lights-out/index.html"
+  }
+];
